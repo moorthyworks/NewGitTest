06358c72f80553b7d0eac90faa7d72f107bd605b
moorthyworks <moorthyworks@gmail.com>
2014-06-20 21:46:10 +0530

    Initial

---
+++ js/my_script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('div.banner>ul').bxSlider({
+		auto: true,
+		pause: 1500,
+		autoHover: true,
+		//autoControls: true,
+		//captions: true,
+		mode:'horizontal'		
+	});
+});
